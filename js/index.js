85f799c0d3f9c43dc38c2fa42f715570e83ce038
github username <github email address>
2020-02-24 17:31:36 -0500

    Done.

--- js/index.js
+++ js/index.js
@@ -1,38 +1,30 @@
 document.addEventListener("DOMContentLoaded", function() {
-    const listPanel = document.getElementById('list-panel')
+    const list = document.getElementById('list')
     const showPanel = document.getElementById('show-panel')
-    const listBooks = function(book){
+    function listBooks(book){
         let bookName = document.createElement('li')
         bookName.innerText = book.title
-        listPanel.appendChild(bookName)
+        list.appendChild(bookName)
         bookName.addEventListener('click', () => {
-            let showBook = document.createElement('div')
             let likeButtonText
-            const user1 = book.users.filter(user => user.username === 'pouros')
-            if (user1[0]){
+            if (book.users.some(user => user.username === 'pouros')){
                 likeButtonText = 'Unlike'
             } else {
                 likeButtonText = 'Like'
             }
-            showBook.innerHTML = 
+            showPanel.innerHTML = 
                 `<h2>${book.title}</h2>
                 <img class='book-image' src = "${book.img_url}"/>
                 <p>${book.description}</p>
                 <button class= 'like-btn' data-id=${book.id}>${likeButtonText}</button>
                 <p>Users who like this book:</p>`
-            const listLike = function(user){
+            function listLike (user){
                 let userLike = document.createElement('li')
                 userLike.innerText = user.username
-                showBook.appendChild(userLike)
+                showPanel.appendChild(userLike)
             }
             book.users.forEach(user => listLike(user))
-            if (showPanel.childElementCount === 0){
-                showPanel.appendChild(showBook)
-            } else {
-                showPanel.innerHTML = ''
-                showPanel.appendChild(showBook)
-            }
-            showBook.addEventListener('click', function(e){
+            showPanel.addEventListener('click', function(e){
                 if (e.target.className === 'like-btn'){
                     e.preventDefault()
                     const user = {"id":1, "username":"pouros"}
@@ -42,25 +34,23 @@ document.addEventListener("DOMContentLoaded", function() {
                         fetch(`http://localhost:3000/books/${e.target.dataset.id}`,{
                         method: "PATCH",
                         headers: {
-                            "content-type": "application/json",
-                            accept: "application/json"
+                            "content-type": "application/json"
                         },
                         body: JSON.stringify({'users':userLikes})
                         })
-                        listLike(user)
-                        e.target.innerText = 'Unlike'
+                        .then(listLike(user))
+                        .then(e.target.innerText = 'Unlike')
                     } else if (e.target.innerText === 'Unlike'){
                         const newLikes = userLikes.filter(user => user.username !== 'pouros')
                         fetch(`http://localhost:3000/books/${e.target.dataset.id}`,{
                         method: "PATCH",
                         headers: {
-                            "content-type": "application/json",
-                            accept: "application/json"
+                            "content-type": "application/json"
                         },
                         body: JSON.stringify({'users':newLikes})
                         })
-                        showBook.removeChild(showBook.lastChild)
-                        e.target.innerText = 'Like'
+                        .then(showPanel.removeChild(showPanel.lastChild))
+                        .then(e.target.innerText = 'Like')
                     }
                 }
             })
