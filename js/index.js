ff74f502b143fdfa430eeb534c1d869a4a3825be
mdiaz26 <mike.diaz006@gmail.com>
2020-03-17 21:30:36 -0400

    practice with the guyz

--- js/index.js
+++ js/index.js
@@ -1,105 +1,85 @@
-const user = {"id":1, "username":"pouros"}
+document.addEventListener("DOMContentLoaded", function(){
+    BOOKS_URL = "http://localhost:3000/books"
+    bookList = document.getElementById('list')
+    showPanel = document.getElementById('show-panel')
+    
+    fetchAndRenderBooks()
+    addBookClickListener()
+})
 
-document.addEventListener("DOMContentLoaded", function() {
-    fetchBooks(),
-    bookClickListener()
-});
-
-function fetchBooks() {
-    fetch("http://localhost:3000/books")
+//get a list of books and render them
+function fetchAndRenderBooks(){
+    fetch(BOOKS_URL)
     .then(resp => resp.json())
-    .then(json => json.forEach(book => renderBook(book)))
+    .then(json => (json.forEach(book => renderBook(book))))
 }
 
-function renderBook(book) {
-    const ul = document.getElementById("list")
-    const li = document.createElement('li')
-    // li.dataset.id = book.id
-    li.innerHTML = `
-    <p data-id="${book.id}">${book.title}</p>
-    `
-    ul.append(li)
+function renderBook(book){
+    let li = document.createElement('li')
+    li.className = "list-element"
+    li.dataset.id = book.id
+    li.innerText = book.title
+    bookList.append(li)
 }
 
-function bookClickListener() {
+function addBookClickListener(){
     document.addEventListener("click", function(event){
-        if (event.target.dataset.id) {
-            fetchBookInfo(event.target.dataset.id)
-        }
-        if (event.target.className === "like-button") {
-            likeBook(event.target, user)
+        if (event.target.className === "list-element"){
+            const bookId = event.target.dataset.id
+            fetch(BOOKS_URL + "/" + bookId)
+            .then(resp => resp.json())
+            .then(json => {
+                console.log(json)
+                renderFullBookInfo(json)})
+        } else if (event.target.className === "like-button"){
+            console.log(event.target.parentNode.dataset.id)
+            const bookId = event.target.parentNode.dataset.id
+            addUserToBook(bookId)
         }
     })
 }
 
-function fetchBookInfo(bookId) {
-    fetch(`http://localhost:3000/books/${bookId}`)
-    .then(resp => resp.json())
-    .then(json => fullBookRender(json))
-}
-
-function fullBookRender(bookInfo) {
-    const bookDiv = document.getElementById("show-panel")
-    bookDiv.innerHTML = ""
-    const display = document.createElement('div')
-    console.log(bookInfo)
-    display.dataset.id = bookInfo.id
-    display.innerHTML = `
-    <h1>${bookInfo.title}</h1>
-    <img src=${bookInfo.img_url}>
-    <p>${bookInfo.description}</p>
-    <ul id="user-likes">Users who like this book:</ul>
-    <button class="like-button">Read Book</button>
+function renderFullBookInfo(book){
+    showPanel.innerHTML = ""
+    let bookDiv = document.createElement('div')
+    bookDiv.dataset.id = book.id
+    bookDiv.innerHTML = `
+    <h1>${book.title}</h1>
+    <img src=${book.img_url}>
+    <p>${book.description}</p>
+    <ul class="users-ul">Users:</ul>
+    <button class="like-button">Like Book</button>
     `
-    const ul = document.createElement('ul')
-    console.log(display)
-    bookInfo.users.forEach(user => liRender(user, ul))
-    display.append(ul)
-    bookDiv.append(display)
-}
-
-function likeBook(button, user) {
-    const bookId = button.parentNode.dataset.id
-    fetch(`http://localhost:3000/books/${bookId}`)
-    .then(resp => resp.json())
-    .then(json => {
-        if (usersToIds(json.users).includes(user.id)) {
-            const usersList = json.users
-            const userIndex = json.users.indexOf(user)
-            usersList.splice(userIndex, 1)
-            patchUser(bookId, usersList)
-        }
-        else {
-            const usersList = json.users
-            usersList.push(user)
-            patchUser(bookId, usersList)
-        }
+    showPanel.append(bookDiv)
+    book.users.forEach(user => {
+        let usersUl = document.getElementsByClassName('users-ul')[0]
+        let userLi = document.createElement('li')
+        userLi.innerText = user.username
+        usersUl.append(userLi)
     })
-
 }
 
-function patchUser(bookId, usersList){
-    console.log(usersList)
-    fetch(`http://localhost:3000/books/${bookId}`, {
+function addUserToBook(bookId){
+    const configObj = {
         method: "PATCH",
         headers: {
-            'Content-Type': 'application/json'
+            "content-type": "application/json",
+            "accept": "application/json"
         },
-        body: JSON.stringify({
-            users: usersList
-        })
-    })
-    fetchBookInfo(bookId)
+        body: JSON.stringify({users: [{"id":1, "username":"pouros"}]})
+    }
+    
+    fetch(BOOKS_URL + "/" + bookId, configObj)
+    .then(resp => resp.json())
+    .then(book => book.users.forEach(user => renderNewLikesList(user)))
 }
 
-function usersToIds(users) {
-    let ids = []
-    users.forEach(user => ids.push(user.id))
-    return ids
+function renderNewLikesList(userObject){
+    let usersUl = document.getElementsByClassName('users-ul')[0]
+    let userLi = document.createElement('li')
+    userLi.innerText = userObject.username
+    usersUl.append(userLi)
 }
+// like book button
 
-function liRender(user, ul) {
-    const li = document.createElement('li')
-    li.innerHTML = `${user.username}`
-    ul.append(li)
-}
+//Bonus: clicking the like button twice "un-likes" the book
